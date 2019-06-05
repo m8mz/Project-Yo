03b5f87c0918f0c431168c194abb2462a82575c8
U-EIGI\mhancock-gaillard <mhancock-gaillard@TEM-MJ060J8Q-W.corp.endurance.com>
2019-06-05 13:55:04 -0700

    updating readme & added spfndkim script

--- install.sh
+++ install.sh
@@ -6,4 +6,9 @@ echo "export PERL5LIB=\"${HOME}/local/lib:${PERL5LIB}\"" >> ${HOME}/.bashrc
 mkdir -p ${HOME}/local/lib
 mkdir -p ${HOME}/local/cookies
 
-wget -qO - 
+echo "Installing.."
+wget -O ${HOME}/local/lib/CSFE.pm https://raw.githubusercontent.com/marcushg36/Project-Yo/master/lib/CSFE.pm
+wget -O ${HOME}/local/lib/vDeck.pm https://raw.githubusercontent.com/marcushg36/Project-Yo/master/lib/vDeck.pm
+wget -O ${HOME}/bin/yo https://raw.githubusercontent.com/marcushg36/Project-Yo/master/yo && chmod +x ${HOME}/bin/yo
+
+echo "Install has finished!"
